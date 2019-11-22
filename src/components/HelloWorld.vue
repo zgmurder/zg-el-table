fa908cb31358fc160d55b82bd10c4c3b9c87259c
zgmurder <294069733@qq.com>
2019-11-22 14:52:42 +0800

    aa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,120 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <lb-table :column="tableData.column"
+      :data="tableData.data"
+      :row-class-name="rowClassName"
+      height="300"
+      border>
+    </lb-table>
   </div>
 </template>
 
 <script>
+import LbTable from './lb-table/lb-table'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    LbTable
+  },
+  data () {
+    return {
+      tableData: {
+        column: [
+          {
+            prop: 'date',
+            label: '日期'
+          },
+          {
+            label: '配送信息',
+            children: [
+              {
+                prop: 'name',
+                label: '姓名'
+              },
+              {
+                label: '地址',
+                children: [
+                  {
+                    prop: 'province',
+                    label: '省份'
+                  },
+                  {
+                    prop: 'city',
+                    label: '市区'
+                  },
+                  {
+                    prop: 'address',
+                    label: '地址'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        data: [
+          {
+            date: '2016-05-03',
+            name: '王小虎1',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎2',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎3',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎4',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎5',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    rowClassName ({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return 'warning-row'
+      } else if (rowIndex === 3) {
+        return 'success-row'
+      }
+      return ''
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.el-table .warning-row {
+  background: oldlace;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.el-table .success-row {
+  background: #f0f9eb;
 }
 </style>
